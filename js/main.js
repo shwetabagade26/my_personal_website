1230d06c6f81af54fa94ae63851fdfd5241c9d25
Shweta Bagade <shwetambagade26@gmail.com>
2023-10-18 21:53:54 +0530

    js

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@
    $(window).load(function() {
 
       // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+    	$("#loader").fadeOut("fast", function(){
 
         // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+        $("#preloader").delay(30).fadeOut("fast");
 
       });       
 
@@ -199,7 +199,7 @@
 
     	$('html, body').stop().animate({
        	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
+      }, 400, 'swing', function () {
       	window.location.hash = target;
       });
 
